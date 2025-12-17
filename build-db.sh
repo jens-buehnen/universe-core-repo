7891b2f3382f33f90e7e44efb213f2627377321f
fdgdfgdfgfgd <jens.buehnen@outlook.de>
2025-12-17 20:33:59 +0100

    update

--- build-db.sh
+++ build-db.sh
@@ -33,7 +33,7 @@ echo "###################################"
 ## -s: signs the packages
 ## -n: only add new packages not already in database
 ## -R: remove old package files when updating their entry
-repo-add -s -n -R universe.db.tar.gz *.pkg.tar.zst
+repo-add -n -R universe.db.tar.gz *.pkg.tar.zst
 
 # Removing the symlinks because GitLab can't handle them.
 rm universe.db
